74e19d95c60f2070c921e10fff92f6c57c0cb4ba
David Tounn <7319dtounn@gmail.com>
2020-10-29 17:51:59 -0700

    new

--- MathClasses/Colour.cs
+++ MathClasses/Colour.cs
@@ -8,12 +8,27 @@ namespace MathClasses
 {
     public class Colour
     {
-        public UInt32 colour;
+        public static UInt32 colour;
+       
 
         public Colour()
         {
             colour = 0;
         }
+    
+        public void SetRed(byte red)
+        {
+            colour = colour & 0x00ffffff;
+            colour |= (UInt32)red << 24;
+        }
+
+       
+        public byte GetRed()
+        {
+            return (byte)((colour & 0xff000000) >> 24);
+            UInt32 value = colour & 0xff000000;
+        }
+
 
     }
 }
