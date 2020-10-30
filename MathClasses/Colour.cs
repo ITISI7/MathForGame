88edae4b448e329ef3376a81ef1d8ab2f6f83abe
David Tounn <7319dtounn@gmail.com>
2020-10-30 10:02:09 -0700

    new

--- MathClasses/Colour.cs
+++ MathClasses/Colour.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,26 +9,62 @@ namespace MathClasses
 {
     public class Colour
     {
-        public static UInt32 colour;
+        public UInt32 colour;
        
 
         public Colour()
         {
-            colour = 0;
+            
         }
-    
+        public Colour(byte red, byte green, byte blue, byte alpha)
+        {
+
+        }
+
+        public byte GetRed()
+        {
+            UInt32 value = colour & 0xff000000;
+            return (byte)((value) >> 24);
+            
+        }
         public void SetRed(byte red)
         {
             colour = colour & 0x00ffffff;
             colour |= (UInt32)red << 24;
         }
-
-       
-        public byte GetRed()
+        
+        public byte GetGreen()
         {
-            return (byte)((colour & 0xff000000) >> 24);
             UInt32 value = colour & 0xff000000;
+            return (byte)((value) >> 24);
         }
+        public void SetGreen(byte green)
+        {
+            colour = colour & 0x00ffffff;
+            colour |= (UInt32)green << 24;
+        }
+        public byte GetBlue()
+        {
+            UInt32 value = colour & 0xff000000;
+            return (byte)((value) >> 24);
+        }
+        public void SetBlue(byte blue)
+        {
+            colour = colour & 0x00ffffff;
+            colour |= (UInt32)blue << 24;
+        }
+        public byte GetAlpha()
+        {
+            UInt32 value = colour & 0xff000000;
+            return (byte)((value) >> 24);
+        }
+        public void SetAlpha(byte alpha)
+        {
+            colour = colour & 0x00ffffff;
+            colour |= (UInt32)alpha << 24;
+        }
+       
+       
 
 
     }
